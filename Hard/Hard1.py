7d4e53a3f8587cb752a0dbcfbe817d034d26184d
Billy Rutecki <wjrutecki@gmail.com>
2021-01-29 15:20:47 -0500

    Change: reorganize files

---
+++ Hard/Hard1.py
@@ -0,0 +1,4 @@
+# we all know the classic "guessing game" with higher or lower prompts.
+# a program that will guess numbers between 1-100,
+# and respond appropriately based on whether users say that the number is too high or too low
+
